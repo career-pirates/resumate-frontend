498dd73d0b7cb7f89e69608e6ec234ef2308c6e1
Seeun Jung <seeun.jung01@gmail.com>
2025-08-27 10:55:26 +0900

    feat: 폴더 섹션 아이콘 추가

--- src/components/Sidebar/FolderTree.tsx
+++ src/components/Sidebar/FolderTree.tsx
@@ -5,7 +5,7 @@ import FolderTreeNode from './FolderTreeNode'
 function FolderTree({ folders }: { folders: Folder[] }) {
   const tree = buildFolderTree(folders)
   return (
-    <div className="text-sm">
+    <div className="text-[20px] ml-6 mt-2 mr-2 mb-2">
       {tree.map((node) => (
         <FolderTreeNode
           key={node.id}
