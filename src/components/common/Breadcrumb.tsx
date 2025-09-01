ac1f4ff83354ae27b66b37e6c2973928a32302bc
Seeun Jung <seeun.jung01@gmail.com>
2025-09-01 10:51:14 +0900

    feat: 반응형 비율 중간 점검

--- src/components/common/Breadcrumb.tsx
+++ src/components/common/Breadcrumb.tsx
@@ -36,8 +36,8 @@ function Breadcrumb() {
   ]
 
   return (
-    <div className="py-4">
-      <div className="text-[var(--label--subtle)] text-sm font-normal flex gap-2">
+    <div className="py-2">
+      <div className="text-[var(--label--subtle)] text-xs font-normal flex gap-2">
         {fullBreadcrumbs.map((breadcrumb, index) => (
           <span key={breadcrumb.id?.toString() || 'home'}>
             <span>{breadcrumb.name}</span>
@@ -52,58 +52,3 @@ function Breadcrumb() {
 }
 
 export default Breadcrumb
-
-// import { useParams } from 'react-router-dom'
-// import { folders } from '../../mockData/folderData'
-
-// interface BreadcrumbProps {
-//   id: bigint | null
-//   name: string
-//   path: string
-// }
-
-// function Breadcrumb() {
-//   const { id } = useParams<{ id: string }>()
-//   const currentFolderId = id ? BigInt(id) : null
-//   const buildBreadcrumbPath = (folderId: bigint | null): BreadcrumbProps[] => {
-//     if (!folderId) return []
-//     const currentFolder = folders.find((f) => f.id === folderId)
-//     if (!currentFolder) return []
-//     const breadcrumbs: BreadcrumbProps[] = []
-
-//     breadcrumbs.unshift({
-//       id: currentFolder.id,
-//       name: currentFolder.name,
-//       path: `/retrospectives/${currentFolder.id}`,
-//     })
-
-//     if (currentFolder.parent_id) {
-//       const parentBreadcrumbs = buildBreadcrumbPath(currentFolder.parent_id)
-//       breadcrumbs.unshift(...parentBreadcrumbs)
-//     }
-//     return breadcrumbs
-//   }
-
-//   const breadcrumbs = buildBreadcrumbPath(currentFolderId)
-//   const fullBreadcrumbs: BreadcrumbProps[] = [
-//     { id: null, name: '홈', path: '/' },
-//     ...breadcrumbs,
-//   ]
-
-//   return (
-//     <div className="py-4">
-//       <div className="text-[var(--label--subtle)] text-[14px] font-normal">
-//         {fullBreadcrumbs.map((breadcrumb, index) => (
-//           <span key={breadcrumb.id?.toString() || 'home'}>
-//             <span>{breadcrumb.name}</span>
-//             {index < fullBreadcrumbs.length - 1 && (
-//               <span className="mx-2">{'>'}</span>
-//             )}
-//           </span>
-//         ))}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Breadcrumb
